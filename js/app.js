03f92822faec511b71e06d5677c2e04d52b491bd
jeanmw <jean.weatherwax@gmail.com>
2017-02-27 17:30:54 -0800

    jean comments

--- js/app.js
+++ js/app.js
@@ -3,15 +3,20 @@ console.log('Hey, js file is connected!');
 // wait for the DOM to finish loading
 $(document).ready(function() {
   var symbol = "O";
+  //thank you for using good variable names
   var hasWon = false;
   var allTurns = ["#", "#", "#", "#", "#", "#", "#", "#", "#"];
 
    function playerTurn(symbol, id){
+     //neat jquery usage. could you have also assigned a class to each space
+     //and checked the class for each spot? Then you could avoid the weird "#" concatenation.
+     //this works though!
      var spotTaken = $("#"+id).text();
      //Freeze the board when someone won
      if(hasWon){
        return false;
      }
+     //you really like to use unicode values
      else if(spotTaken == '\xa0') {
        //Place the X and O at the correct position in the array
        allTurns.splice(parseInt(id), 1, symbol);
@@ -25,6 +30,7 @@ $(document).ready(function() {
       window.location.reload();
  });
 
+ //nice!
    function checkWin(turnArray, turn){
      if((turnArray[0] === turn && turnArray[1] === turn && turnArray[2] === turn) ||
         (turnArray[0] === turn && turnArray[3] === turn && turnArray[6] === turn) ||
